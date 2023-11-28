2abc79261328906a512b63a99252688493d49380
Andrés Weitzel <andres96energy@hotmail.com>
2023-11-27 23:54:02 -0300

    Added and checked helper

--- src/config/routes/images-routes.ts
+++ src/config/routes/images-routes.ts
@@ -1,12 +1,12 @@
 //External
-import {Router} from 'express';
+import { Router } from "express";
 const multer = require("multer");
+import "dotenv/config";
 //Controllers
-import { uploadImageController } from '../../controllers/images/upload';
-//Config
-const upload = multer({ dest: "assets/images/uploads/" });
-//Const-vars
+import { uploadImageController } from "../../controllers/images/upload";
+//Const
+const IMG_PATH_NAME_BASE_URL: string = process.env.IMG_PATH_NAME_BASE_URL || "";
+const upload = multer({ dest: IMG_PATH_NAME_BASE_URL });
 export const imagesRouter = Router();
 
 imagesRouter.post("/", upload.single("img"), uploadImageController);
-
--- src/controllers/images/upload.ts
+++ src/controllers/images/upload.ts
@@ -1,6 +1,13 @@
 //External
 import { Request, Response } from "express";
 import "dotenv/config";
+//Helpers
+import { renameFile } from "../../helpers/files/renameFile";
+//Vars
+let file: any;
+let renameFileResult: any;
+let msgResponse: string;
+let msgLog: string;
 /**
  * @description Controller to upload an image. For using Multer with typescript is necessary using the type any for req param
  * @param {any} req any type
@@ -8,13 +15,20 @@ import "dotenv/config";
  * @returns  an object with the image information
  * @example
  */
-export const uploadImageController = async (req: any , res: Response) => {
+export const uploadImageController = async (req: any, res: Response) => {
   try {
-    console.log(req.file);
-    return res
-    .status(200)
-    .send("OK");
-  } catch (error) {}
+    file = req.file;
+    if (file == null || file == undefined) {
+      return res.status(400).send("Bad request, could not get image");
+    }
+    renameFileResult = await renameFile(file);
+    if (renameFileResult == null || renameFileResult == undefined) {
+      return res.status(400).send("Bad request, image has not been processed, but it has been stored");
+    }
+    return res.status(200).send(file);
+  } catch (error) {
+    msgResponse = "ERROR in uploadImageController() function.";
+    msgLog = msgResponse + `Caused by ${error}`;
+    console.log(msgLog);
+  }
 };
-
-
